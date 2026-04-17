f32a006fcfc4fdb9b855093c39244f9d4eb6d22b
ali-tmam <malihaider19967@gmail.com>
2026-04-17 17:43:16 +0400

    asdfasdf

--- resources/views/livewire/pages/page-detail.blade.php
+++ resources/views/livewire/pages/page-detail.blade.php
@@ -1,4 +1,4 @@
-<div class="space-y-6">
+<div class="space-y-6" wire:init="autoGenerateSnippet">
     <div>
         <a href="{{ route('pages.index') }}" class="inline-flex items-center gap-1 text-xs text-slate-500 transition hover:text-indigo-600 dark:text-slate-400 dark:hover:text-indigo-400">
             <svg class="h-3.5 w-3.5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" /></svg>
