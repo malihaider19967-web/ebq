e257c16beb2d90aa2d59dc4d9e0013b54bd0fad5
ali-tmam <malihaider19967@gmail.com>
2026-04-28 21:28:14 +0400

    asdfsd

--- resources/views/partials/favicon-links.blade.php
+++ resources/views/partials/favicon-links.blade.php
@@ -1,3 +1,3 @@
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32.png') }}">
-<link rel="icon" type="image/png" sizes="48x48" href="{{ asset('apple-touch-icon.png') }}">
-<link rel="apple-touch-icon" href="{{ asset('apple-touch-icon-180.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('logo.png') }}?v={{ @filemtime(public_path('logo.png')) }}">
+<link rel="icon" type="image/png" sizes="48x48" href="{{ asset('logo.png') }}?v={{ @filemtime(public_path('logo.png')) }}">
+<link rel="apple-touch-icon" href="{{ asset('logo.png') }}?v={{ @filemtime(public_path('logo.png')) }}">
